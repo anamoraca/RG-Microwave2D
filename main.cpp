bfc849625593807d178ddcca905132dd10c6d351
Ana <anamoraca8@gmail.com>
2024-12-02 00:40:35 +0100

    Text rendering git add .!

--- main.cpp
+++ main.cpp
@@ -20,11 +20,16 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 unsigned int compileShader(GLenum type, const char* source);
 unsigned int createShader(const char* vsSource, const char* fsSource);
 unsigned int initVAO(float* vertices, unsigned int* indices, int vertexSize, int indicesSize, int strideV);
-//void initFreeType(FT_Library& ft, FT_Face& face);
-//void RenderText(const std::string& text, float x, float y, float scale, glm::vec3 color, unsigned int shaderProgram);
+void loadFont(const char* fontPath);
+void RenderText(unsigned int shader, const std::string& text, float x, float y, float scale, glm::vec3 color, unsigned int& VAO, unsigned int& VBO);
+
 
 struct Character {
     unsigned int TextureID;  // ID teksture
@@ -34,12 +39,13 @@ struct Character {
 };
 
 std::map<char, Character> Characters;
-unsigned int VAO, VBO;
 
 
 int main(void)
 {
-    FT_Library ft;
+    
+   
+
     if (!glfwInit())
     {
         std::cout << "GLFW Biblioteka se nije ucitala! :(\n";
@@ -247,6 +253,21 @@ int main(void)
     doorFrameVAO = initVAO(doorFrameVertices, uniIndices, sizeof(doorFrameVertices), sizeof(uniIndices), 5 * sizeof(float));
     doorVAO = initVAO(doorVertices, uniIndices, sizeof(doorVertices), sizeof(uniIndices), 5 * sizeof(float));
     
+    // TExt
+    unsigned int textVAO, textVBO;
+        glGenVertexArrays(1, &textVAO);
+        glGenBuffers(1, &textVBO);
+        glBindVertexArray(textVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, textVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+
+        loadFont("ARIAL.TTF");
+
+
     unsigned int texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
@@ -274,13 +295,14 @@ int main(void)
     }
 
     unsigned int basicShader = createShader("basic.vert", "basic.frag");
+    unsigned int textShader = createShader("text.vert", "text.frag");
+
+    glm::mat4 projection = glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f);
+    glUseProgram(textShader);
+    glUniformMatrix4fv(glGetUniformLocation(textShader, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
     glClearColor(0.960784, 0.960784, 0.862745, 1.0);
 
-
-    /*FT_Library ft;
-    FT_Face face;
-    initFreeType(ft, face);*/
     int minutes = 2, seconds = 30;
 
     while (!glfwWindowShouldClose(window))
@@ -347,12 +369,20 @@ int main(void)
             glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         }
 
-        // Iscrtavanje brojeva tastature
+         //Iscrtavanje brojeva tastature
       /*  for (int i = 0; i < 10; ++i) {
             char numStr[2];
             snprintf(numStr, sizeof(numStr), "%d", i);
             RenderText(numStr, numberButtonVertices[i][10] + 0.02f, numberButtonVertices[i][11] + 0.02f, 0.5f, glm::vec3(0.8f, 0.8f, 0.8f), basicShader);
         }*/
+
+        glBindVertexArray(textVAO);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+        RenderText(textShader, "A A A A ", -0.8,-0.8, 0.005, glm::vec3(0.0f, 0.0f, 0.0f), textVAO, textVBO);
+    
+        
 
         glBindVertexArray(0);
         glUseProgram(0);
@@ -408,128 +438,94 @@ unsigned int initVAO(float* vertices, unsigned int* indices,int vertexSize, int 
 }
 
 
+void loadFont(const char* fontPath) {
+    FT_Library ft;
+    if (FT_Init_FreeType(&ft)) {
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+        return;
+    }
+
+    FT_Face face;
+    if (FT_New_Face(ft, fontPath, 0, &face)) {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+        return;
+    }
+
+    FT_Set_Pixel_Sizes(face, 0, 48);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    // Učitavanje svih ASCII karaktera
+    for (unsigned char c = 0; c < 128; c++) {
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+            std::cout << "ERROR::FREETYPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+
+        unsigned int texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D, 0, GL_RED,
+            face->glyph->bitmap.width, face->glyph->bitmap.rows,
+            0, GL_RED, GL_UNSIGNED_BYTE, face->glyph->bitmap.buffer
+        );
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        Character character = {
+            texture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            face->glyph->advance.x
+        };
+        Characters.insert(std::pair<char, Character>(c, character));
+    }
+
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+}
 
 
-//void initFreeType(FT_Library& ft, FT_Face& face) {
-//    if (FT_Init_FreeType(&ft)) {
-//        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
-//        exit(1);
-//    }
-//
-//    if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face)) {
-//        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
-//        exit(1);
-//    }
-//
-//    FT_Set_Pixel_Sizes(face, 0, 48); // Postavi visinu fonta na 48 piksela
-//
-//    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // Bez ogranicenja poravnanja bajta
-//
-//    for (unsigned char c = 0; c < 128; c++) {
-//        if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
-//            std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
-//            continue;
-//        }
-//
-//        unsigned int texture;
-//        glGenTextures(1, &texture);
-//        glBindTexture(GL_TEXTURE_2D, texture);
-//        glTexImage2D(
-//            GL_TEXTURE_2D,
-//            0,
-//            GL_RED,
-//            face->glyph->bitmap.width,
-//            face->glyph->bitmap.rows,
-//            0,
-//            GL_RED,
-//            GL_UNSIGNED_BYTE,
-//            face->glyph->bitmap.buffer
-//        );
-//
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//
-//        Character character = {
-//            texture,
-//            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
-//            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
-//            face->glyph->advance.x
-//        };
-//        Characters.insert(std::pair<char, Character>(c, character));
-//    }
-//
-//    glBindTexture(GL_TEXTURE_2D, 0);
-//
-//    FT_Done_Face(face);
-//    FT_Done_FreeType(ft);
-//
-//    glGenVertexArrays(1, &VAO);
-//    glGenBuffers(1, &VBO);
-//    glBindVertexArray(VAO);
-//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-//    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
-//    glEnableVertexAttribArray(0);
-//    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
-//}
+void RenderText(unsigned int shader, const std::string& text, float x, float y, float scale, glm::vec3 color, unsigned int& VAO, unsigned int& VBO) {
+    glUseProgram(shader);
 
+    glUniform3f(glGetUniformLocation(shader, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAO);
 
-// FUNKCIJA ZA RENDEROVANJE TEKSTA
+    for (char c : text) {
+        Character ch = Characters[c];
 
-//void RenderText(const std::string& text, float x, float y, float scale, glm::vec3 color, unsigned int shaderProgram) {
-//    glUseProgram(shaderProgram);
-//    glUniform3f(glGetUniformLocation(shaderProgram, "textColor"), color.x, color.y, color.z);
-//    glActiveTexture(GL_TEXTURE0);
-//    glBindVertexArray(VAO);
-//
-//    for (const char& c : text) {
-//        Character ch = Characters[c];
-//
-//        float xpos = x + ch.Bearing.x * scale;
-//        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
-//
-//        float w = ch.Size.x * scale;
-//        float h = ch.Size.y * scale;
-//
-//        float vertices[6][4] = {
-//            { xpos,     ypos + h,   0.0f, 0.0f },
-//            { xpos,     ypos,       0.0f, 1.0f },
-//            { xpos + w, ypos,       1.0f, 1.0f },
-//
-//            { xpos,     ypos + h,   0.0f, 0.0f },
-//            { xpos + w, ypos,       1.0f, 1.0f },
-//            { xpos + w, ypos + h,   1.0f, 0.0f }
-//        };
-//
-//        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
-//        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-//        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
-//        glBindBuffer(GL_ARRAY_BUFFER, 0);
-//        glDrawArrays(GL_TRIANGLES, 0, 6);
-//
-//        x += (ch.Advance >> 6) * scale;
-//    }
-//    glBindVertexArray(0);
-//    glBindTexture(GL_TEXTURE_2D, 0);
-//}
+        float xpos = x + ch.Bearing.x * scale;
+        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
 
+        float w = ch.Size.x * scale;
+        float h = ch.Size.y * scale;
 
+        float vertices[6][4] = {
+            { xpos,     ypos + h,   0.0f, 0.0f },
+            { xpos,     ypos,       0.0f, 1.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
 
+            { xpos,     ypos + h,   0.0f, 0.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
+            { xpos + w, ypos + h,   1.0f, 0.0f }
+        };
 
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
+        x += (ch.Advance >> 6) * scale;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
 
 unsigned int compileShader(GLenum type, const char* source)
 {
